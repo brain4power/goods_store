f748d27f0ff2908432ce87476fe2e594d51377de
brain4power <yotakrasnodar@yandex.ru>
2019-08-16 09:51:41 +0300

    add media path for debug mode

--- goods_store/urls.py
+++ goods_store/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_swagger_view(title='Little brave API')
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('api/v1/', include('mainapp.urls')),
     path('swagger/', schema_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
