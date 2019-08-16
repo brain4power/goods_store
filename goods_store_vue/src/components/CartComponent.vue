5358e6360094de3a6039d0719b033b1c70cad575
brain4power <yotakrasnodar@yandex.ru>
2019-08-16 14:09:25 +0300

    make base cart and product-for-cart components

--- goods_store_vue/src/components/CartComponent.vue
+++ goods_store_vue/src/components/CartComponent.vue
@@ -1,13 +1,80 @@
 <template>
-    
+    <div class="cart">
+        <HeaderComponent></HeaderComponent>
+        <div class="container">
+            <div class="row cart_header">
+                <div class="d-flex justify-content-between align-items-center col-lg-8 col-md-8">
+                    <span class="row cart_name">Корзина</span>
+                    <vs-button color="primary" type="line" icon="delete_forever" size="small" class="row cart__del_all">Удалить все</vs-button>
+                </div>
+            </div>
+            <div class="row cart__products__container d-flex justify-content-between">
+                <div class="cart__products col-lg-8 col-md-8">
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                    <ProductCardCartComponent></ProductCardCartComponent>
+                </div>
+                <div class="cart__summary col-lg-3 col-md-3">
+                    <div class="d-flex justify-content-between">
+                        <span class="">Итого:</span>
+                        <span class="font-weight-bold">2340р</span>
+                    </div>
+                    <div>
+                        <vs-button color="primary" type="filled" class="button_checkout">Заказать</vs-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import HeaderComponent from "./HeaderComponent";
+    import ProductCardCartComponent from "./ProductCardCartComponent";
     export default {
-        name: "CartComponent"
+        name: "CartComponent",
+        components: {ProductCardCartComponent, HeaderComponent},
+        comments: {
+            HeaderComponent: () => import('./HeaderComponent.vue'),
+            ProductCardCartComponent: () => import('./ProductCardCartComponent.vue'),
+        }
     }
 </script>
 
 <style scoped>
-
+    .cart_header {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    .cart_name {
+        font-size: 20px;
+    }
+    .cart__del_all {
+        /*height: 30px;*/
+    }
+    .cart__products__container {
+    }
+    .cart__summary {
+        position: sticky;
+        top: 0;
+        height: 110px;
+        padding: 10px;
+        border: 1px solid #e5e5e5;
+        border-radius: 5px;
+        background: white;
+    }
+    @media (max-width: 768px) {
+        .cart__summary {
+            bottom: 0;
+        }
+    }
+    .button_checkout {
+        width: 100%;
+        margin-top: 20px;
+    }
 </style>
---
+++ goods_store_vue/src/components/ProductCardCartComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="productCart row d-flex align-items-center">
+        <div class="productCart__del col-lg-1 col-2 d-flex align-items-center justify-content-center">
+            <vs-icon icon="delete_forever" color="primary"></vs-icon>
+        </div>
+        <div class="productCart__picture col-lg-2 col-3">
+            <a href="#" class="temp_img">
+                <!--<img :src="img">-->
+            </a>
+        </div>
+        <div class="productCart__title d-inline-block col-lg-5 col-7">
+            <div class="productCart__title__name">{{product_title_name}}</div>
+            <div class="productCart__title__article">артикул: {{product_title_article}}</div>
+            <div class="productCart__title__price">{{product_title_price}}р/шт</div>
+        </div>
+        <div class="productCart__count col-lg-2 col-7">
+            <div class="d-flex align-items-center justify-content-center">
+                <vs-input-number v-model="number1"/>
+            </div>
+        </div>
+        <div class="productCart__total-cost col-lg-2 col-5">2900p</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProductCardCartComponent",
+        data: () => ({
+            product_title_name: 'Продукт консерва черноморская',
+            product_title_article: '123008',
+            product_title_price: '213',
+            number1: 21
+
+        })
+    }
+</script>
+
+<style scoped>
+    .productCart {
+        border: 1px solid #e5e5e5;
+        border-radius: 5px;
+        padding: 10px 0 10px 0;
+    }
+
+    .productCart__picture {
+        height: 100px;
+        width: 70px;
+        background-color: #e5e5e5;
+        /*margin: 10px;*/
+    }
+</style>
